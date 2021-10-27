2c1e9a099a56143961998e345728ed75f259847d
praksahb <bhaskar1126@gmail.com>
2021-10-27 19:25:32 +0530

    user authorization added

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -4,6 +4,7 @@ const rootUrl = "http://localhost:3001/v1/";
 const loginUrl = rootUrl + "user/login";
 const userProfileUrl = rootUrl + "user";
 const logoutUrl = rootUrl + "user/logout";
+const newAccessJWTurl = rootUrl + "tokens";
 
 export const userLogin = (frmData) => {
 	return new Promise(async (resolve, reject) => {
@@ -41,6 +42,36 @@ export const fetchUser = () => {
 			resolve(result.data);
 		} catch (error) {
 			reject(error);
+		}
+	});
+};
+
+export const fetchNewAccessJWT = () => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const { refreshJWT } = JSON.parse(localStorage.getItem("crmSite"));
+
+			if (!refreshJWT) {
+				reject("Token not found");
+			}
+			const result = await axios.get(newAccessJWTurl, {
+				headers: {
+					Authorization: refreshJWT,
+				},
+			});
+			console.log(result.data);
+			if (result.data.status === "success") {
+				sessionStorage.setItem("accessJWT", result.data.accessJWT);
+			}
+
+			//console.log(result.data);
+			resolve(true);
+		} catch (error) {
+			if (error.message === "Request failed with status code 403") {
+				localStorage.removeItem("crmSite");
+			}
+			console.log(error.message);
+			reject(false);
 		}
 	});
 };
